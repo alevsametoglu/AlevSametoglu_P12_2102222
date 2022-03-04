cbe7724d9f266f8772d9dbe81877c3016c72fe4e
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-03-04 11:22:00 +0100

    add props chilren

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import Header from "../components/Header/Header";
-import SideBar from "../components/SideBar/SideBar";
+import React from 'react'
+import { Outlet } from 'react-router'
+import Header from '../components/Header/Header'
+import SideBar from '../components/SideBar/SideBar'
 
 export default function MainLayout(props) {
-  return (
-    <>
-      <Header />
-      <div style={{ display: "flex" }}>
-        <SideBar />
-        <Outlet />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <div style={{ display: 'flex' }}>
+                <SideBar />
+                {props.children}
+            </div>
+        </>
+    )
 }
