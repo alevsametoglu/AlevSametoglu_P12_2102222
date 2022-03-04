5eac15c87f0977d256edf99621d4e1edc1807e4f
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-03-04 11:21:31 +0100

    add link to icon

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,25 +1,34 @@
-import React from "react";
-import PropTypes from "prop-types";
-import "./SideBar.scss";
-import { ReactComponent as YogaIcon } from "../../assets/images/YogaIcon.svg";
-import { ReactComponent as SwimIcon } from "../../assets/images/SwimIcon.svg";
-import { ReactComponent as BodyBuildIcon } from "../../assets/images/BodyBuildIcon.svg";
-import { ReactComponent as BycIcon } from "../../assets/images/BycIcon.svg";
+import React from 'react'
+import PropTypes from 'prop-types'
+import './SideBar.scss'
+import { ReactComponent as YogaIcon } from '../../assets/images/YogaIcon.svg'
+import { ReactComponent as SwimIcon } from '../../assets/images/SwimIcon.svg'
+import { ReactComponent as BodyBuildIcon } from '../../assets/images/BodyBuildIcon.svg'
+import { ReactComponent as BycIcon } from '../../assets/images/BycIcon.svg'
+import { Link } from 'react-router-dom'
 
 const SideBar = (props) => {
-  return (
-    <aside className="side-bar">
-      <nav>
-        <YogaIcon className="icons" />
-        <SwimIcon className="icons" />
-        <BodyBuildIcon className="icons" />
-        <BycIcon className="icons" />
-      </nav>
-      <footer>Copyright, SportSee 2020</footer>
-    </aside>
-  );
-};
+    return (
+        <aside className="side-bar">
+            <nav>
+                <Link to="./yoga">
+                    <YogaIcon className="icons" />
+                </Link>
+                <Link to="swimming">
+                    <SwimIcon className="icons" />
+                </Link>
+                <Link to="./bodyBuilding">
+                    <BodyBuildIcon className="icons" />
+                </Link>
+                <Link to="biking">
+                    <BycIcon className="icons" />
+                </Link>
+            </nav>
+            <footer>Copyright, SportSee 2020</footer>
+        </aside>
+    )
+}
 
-SideBar.propTypes = {};
+SideBar.propTypes = {}
 
-export default SideBar;
+export default SideBar
