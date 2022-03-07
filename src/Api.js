191230c9dd0efc9beaa8605538bd44bd19858137
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-03-07 21:15:11 +0100

    add commentaire js doc

--- src/Api.js
+++ src/Api.js
@@ -1,27 +1,51 @@
 import axios from 'axios'
 axios.defaults.baseURL = `http://localhost:3000`
+/**
+ *@function get data, we use axios to connect
+ * with the backend API.
+ * @param {string} url
+ * @returns data
+ */
 
 const getData = (url) => axios.get(url).then((response) => response.data.data)
 
 export const API = {
-    /**
-     * Returns the sum of a and b
+    /** @function get user info from api
+     *
      * @param {number} userId
-     * @returns {Object} user info
+     * @returns {Object} (user info)
      */
 
     getUser: (userId) => {
         const url = `/user/${userId}`
         return getData(url)
     },
+    /**@function get user performance
+     *
+     * @param {number} userId
+     * @returns {object} (user performance)
+     */
+
     getUserPerformance: (userId) => {
         const url = `user/${userId}/performance`
         return getData(url)
     },
+
+    /**@function get user activity
+     *
+     * @param {number} userId
+     * @returns {object}( user activity)
+     */
+
     getUserActivity: (userId) => {
         const url = `user/${userId}/activity`
         return getData(url)
     },
+    /**@function get user average sessions
+     *
+     * @param {number} userId
+     * @returns {object}( user average sessions)
+     */
     getUserAverageSessions: (userId) => {
         const url = `user/${userId}/average-sessions`
         return getData(url)
