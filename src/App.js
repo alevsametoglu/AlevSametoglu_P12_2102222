a286f6b6fbf6b7f0c44746e97ce7200791c09f96
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-03-07 21:15:39 +0100

    add commentaire js doc and fixed route

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
 import MainLayout from './Layout/MainLayout'
 import Dashboard from './pages/Dashboard'
-
 import ErrorPage from './pages/ErrorPage'
+import ProgressPage from './pages/ProgressPage'
 import UserList from './pages/UserList'
+
+/**
+ * Routes application
+ * @returns {objet} (routes of pages)
+ */
 
 function App() {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<UserList />} />
+                    <Route path="user/:id" element={<Dashboard />} />
+                    <Route path="/reglage" element={<ProgressPage />} />
+                    <Route path="/communite" element={<ProgressPage />} />
+                    <Route path="/yoga" element={<ProgressPage />} />
+                    <Route path="/swimming" element={<ProgressPage />} />
+                    <Route path="/biking" element={<ProgressPage />} />
+                    <Route path="/bodyBuilding" element={<ProgressPage />} />
+                    <Route path="/profil" element={<ProgressPage />} />
+                    <Route path="*" element={<ErrorPage />} />
+                </Route>
                 <Route path="/" index element={<UserList />} />
                 <Route
-                    path="user/:id"
+                    path="/user/:id"
                     element={
                         <MainLayout>
                             <Dashboard />
                         </MainLayout>
                     }
                 />
-                <Route path="/yoga" element={<ErrorPage />} />
-                <Route path="/swimming" element={<ErrorPage />} />
-                <Route path="/biking" element={<ErrorPage />} />
-                <Route path="/bodyBuilding" element={<ErrorPage />} />
-
-                <Route path="*" element={<ErrorPage />} />
+                <Route
+                    path="/profil"
+                    element={
+                        <MainLayout>
+                            <ProgressPage />
+                        </MainLayout>
+                    }
+                />
             </Routes>
         </Router>
     )
